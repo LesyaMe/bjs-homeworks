be7321803b5acbd45453746481eac7ac1d93264c
Olesya <olesya.dedlovskaya@gmail.com>
2019-03-29 20:46:18 +0300

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,30 @@
+//  задача 1
+let x;
+let y;
+let z;
+
+x = 2;
+y = 22;
+z = 0;
+
+result = x * y + 5 * z + x - 1;
+console.log(result);
+
+
+//  задача 2
+let algebra;
+let geography;
+let physics;
+
+algebra = 4;
+geography = 3;
+physics = 5;
+
+averageRating = (algebra + geography + physics) / 3; 
+console.log(averageRating);
+
+
+//  задача 3
+name = 'Olesya';
+
+console.log(`Привет, мир! Меня зовут ${name}`);
